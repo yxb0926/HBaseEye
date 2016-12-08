4d94ad3095997e533b8f41078497ae30a9b53181
袁小斌 <yuanxiaobin@oneniceapp.com>
2016-12-08 10:33:01 +0800

    thrift monitor

---
+++ client/thrift/serverTag.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#encoding:utf-8
+
+'''
+Author: yuanxiaobin
+Email:  yxb0926@163.com
+'''
+
+tagdict = {
+    "RegionServer": "Hadoop:service=HBase,name=RegionServer,sub=Server",
+}
