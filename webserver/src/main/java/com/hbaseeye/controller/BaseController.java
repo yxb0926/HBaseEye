cf9f231f96713a346d2cdd79a7df4bb46654a235
袁小斌 <yuanxiaobin@oneniceapp.com>
2016-12-14 17:27:09 +0800

    pom modify

--- webserver/src/main/java/com/hbaseeye/controller/BaseController.java
+++ webserver/src/main/java/com/hbaseeye/controller/BaseController.java
@@ -1,12 +1,10 @@
 package com.hbaseeye.controller;
 
 import com.hbaseeye.service.RequestService;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/mng")
 public class BaseController {
-    private Logger logger = Logger.getLogger(BaseController.class);
 
     @Autowired
     private RequestService requestService;
